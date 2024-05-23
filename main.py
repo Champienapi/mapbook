b6f83216210a4062593513c1cb6585e386dde67f
Champienapi <168068971+Champienapi@users.noreply.github.com>
2024-05-23 12:16:46 +0200

    wip: add create function

--- main.py
+++ main.py
@@ -12,12 +12,28 @@ print(f'Witaj {data_of_users[0]['name']}')
 
 def read(users: list) -> None:
     """
-    this is a function to show users from an list
+    show users from an list
     :param users: a list of users
     :return: None
     """
     for user in users[1:]:
-        print(f'twój znajomy: {user['name']},opublikował :{user['posts']}')
+        print(f'twój znajomy: {user['name']}, opublikował: {user['posts']}')
 
 
-read(data_of_users)
+# read(data_of_users)
+
+def create(users: list) -> None:
+    """
+    add a user to a list
+    :param users: user list
+    :return: none
+    """
+    name=input("Enter your name: ")
+    surname:str='Szczepaniuk'
+    posts:int=int(input("Enter your number of posts: "))
+    location:str='Mińsk Mazowiecki'
+    new_user: dict={'name': name, 'surname': surname, 'post': posts, 'location': location}
+    users.append(new_user)
+
+# add_user(data_of_users)
+# read(data_of_users)
